97a30d03d1ba3873a1fdffed7be1281dd48b4d76
Aymogul <aymogul22@gmail.com>
2024-08-23 18:59:14 -0700

    upper

---
+++ string-cases.py
@@ -0,0 +1,7 @@
+# Exposing uppercase and lowercase
+
+text = "Python is easy for you and me"
+uppercase = text.upper()
+lowercase = text.lower()
+print("Uppercase:", uppercase)
+print("Lowercase:", lowercase)
